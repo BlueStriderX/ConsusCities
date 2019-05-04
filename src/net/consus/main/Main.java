8899429dba226d5eeb13b519f9d0a658ab001cc8
TheDerpGamer <garret2kool@gmail.com>
2019-05-04 11:46:04 -0700

    Update Main.java

--- src/net/consus/main/Main.java
+++ src/net/consus/main/Main.java
@@ -1,9 +1,15 @@
 package net.consus.main;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import com.massivecraft.factions.entity.Faction;
+import net.consus.cities.City;
 
 public class Main extends JavaPlugin {
 
+public static Multimap<Faction, City> cities = ArrayListMultimap.create();
+	
 private static Main plugin;
   public String NAME = "Consus Cities";
   public String VERSION = "0.1";
